521da4e9be31ef6a42f22865581f10fa7a236b3b
petros_ <p.kontakis@athlenda.com>
2019-08-03 14:33:40 +0300

    add search filter and paging

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { SourceModel } from 'src/app/models/source-model';
+
 enum STATES {
   LOADING,
   SERVER_ERROR,
@@ -16,24 +17,73 @@ export class HomePageComponent implements OnInit {
 
   state: number;
   STATES = STATES;
-  sourceList: Array<SourceModel>;
+  sourceFiltered: Array<SourceModel>;
+  page: number;
+  slectedCategory: string;
+  private sourceList: Array<SourceModel>;
+  private pageStep = 9;
+  private terms: string;
 
-  constructor(public api: ApiService) { }
+
+  constructor(private api: ApiService) {
+    this.page = 0;
+  }
 
   ngOnInit() {
-    console.log(this.api)
+
+
     this.state = STATES.LOADING;
     this.api.getSources().subscribe((response) => {
-      console.log(response);
       if (response.status === "ok") {
         this.state = STATES.NORMAL;
-        return this.sourceList  = response.sources.map((src)=> new SourceModel(src))
+        this.sourceList = response.sources.map((src) => new SourceModel(src))
+        this.setSource(false, "");
+        return;
       }
+      this.state = STATES.SERVER_ERROR;
+
     })
   }
 
-  searchBy() {
-    
+  onSearchChange(value: string) {
+    this.page = 0;
+    this.terms = value;
+    this.setSource(false, value);
+  }
+
+
+
+  setNextPage(lazy: boolean): void {
+    this.page++;
+    this.setSource(lazy, this.terms);
+
+  }
+  setPreviewsPage(): void {
+    this.page--;
+    this.setSource(false, this.terms)
+  }
+
+  setSource(lazy: boolean, terms: string) {
+
+    let sourceFiltered = this.sourceList;
+
+    // filtering by name
+    if (terms) {
+      sourceFiltered = this.sourceList.filter(src => {
+        return src.name.toLowerCase().includes(terms.toLowerCase())
+      })
+    }
+
+    // Paging to data
+    let pageStart: number;
+    if (lazy) {
+      pageStart = 0;
+    } else {
+      pageStart = this.page * this.pageStep;
+
+    }
+    const pageEnd = (this.page + 1) * this.pageStep;
+    this.sourceFiltered = sourceFiltered.slice(pageStart, pageEnd)
   }
 
 }
